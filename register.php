b06b6448601a419844d86b4226909f4202b3a574
BhathiyaTK <bhathiyakariyawasam94@gmail.com>
2018-09-30 22:48:31 +0530

    added front-end jquery form validation

--- register.php
+++ register.php
@@ -24,10 +24,105 @@
 	<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 	<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/jquery.validation/1.15.1/jquery.validate.min.js"></script>
 	<link rel="icon" href="images/VR-logo-1.png">
 	<title>Virtual Rival | Registration</title>
 </head>
 <body>
+	<style type="text/css">
+		input:not([type="file"]).error,
+		textarea.error,
+		select.error {
+		  border: 1px solid red !important;
+		}
+		 
+		input:not([type="file"]).no-error,
+		textarea.no-error,
+		select.no-error {
+		  border: 1px solid green !important;
+		}
+		 
+		form .error {
+		  color: red;
+		  font-size: small;
+		}
+	</style>
+    <script type="text/javascript">
+        $(document).ready(function(){
+            $("#registration_form").validate({
+            	rules: {
+            		team_name: "required",
+            		university_name: "required",
+            		leader_name: "required",
+            		leader_email: "required",
+            		leader_phone: "required",
+            		leader_t_shirt: "required",
+            		leader_food: "required",
+            		member1_name: "required",
+            		member1_email: "required",
+            		member1_t_shirt: "required",
+            		member1_food: "required",
+            		member2_name: "required",
+            		member2_email: "required",
+            		member2_t_shirt: "required",
+            		member2_food: "required",
+            		member3_name: "required",
+            		member3_email: "required",
+            		member3_t_shirt: "required",
+            		member3_food: "required",
+            		member4_name: "required",
+            		member4_email: "required",
+            		member4_t_shirt: "required",
+            		member4_food: "required"
+            	},
+        		messages: {
+			      	team_name: "Team name is required",
+			      	university_name: "University name is required",
+			      	leader_name: "Please enter leader's name",
+			      	leader_email: "Please enter a valid email address",
+			      	leader_phone: "Please enter mobile number",
+			      	leader_t_shirt: "This field is required",
+			      	leader_food: "This field is required",
+			      	member1_name: "Please enter member-01's name",
+			      	member1_email: "Please enter a valid email address",
+			      	member1_t_shirt: "This field is required",
+			      	member1_food: "This field is required",
+			      	member2_name: "Please enter member-02's name",
+			      	member2_email: "Please enter a valid email address",
+			      	member2_t_shirt: "This field is required",
+			      	member2_food: "This field is required",
+			      	member3_name: "Please enter member-03's name",
+			      	member3_email: "Please enter a valid email address",
+			      	member3_t_shirt: "This field is required",
+			      	member3_food: "This field is required",
+			      	member4_name: "Please enter member-04's name",
+			      	member4_email: "Please enter a valid email address",
+			      	member4_t_shirt: "This field is required",
+			      	member4_food: "This field is required",
+			    },
+
+			    submitHandler: function(form) {
+			      	form.submit();
+			    }
+            });
+
+            $("#leader_contact_no").keydown(function (e) {
+		        // Allow: backspace, delete, tab, escape, enter and .
+		        if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110]) !== -1 ||
+		             // Allow: Ctrl+A, Command+A
+		            (e.keyCode === 65 && (e.ctrlKey === true || e.metaKey === true)) || 
+		             // Allow: home, end, left, right, down, up
+		            (e.keyCode >= 35 && e.keyCode <= 40)) {
+		                 // let it happen, don't do anything
+		                 return;
+		        }
+		        // Ensure that it is a number and stop the keypress
+		        if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+		            e.preventDefault();
+		        }
+		    });
+        });
+    </script>
 
 	<div>
 		<div id="particles-js"></div>
@@ -40,7 +135,7 @@
 					</div>
 				</div>
 				<div class="reg-container-div" id="registration-form-div">
-					<form action="process.php" method="POST">
+					<form action="process.php" method="POST" id="registration_form">
 						<!--Team details part-->
 						<div id="required-text-div">
 							* Required Fields
@@ -57,7 +152,7 @@
 					  	<div class="form-group row">
 						    <label for="university_name" class="col-sm-3 col-form-label">University Name<span>*</span></label>
 						    <div class="col-sm-9">
-						      	<input type="text" class="form-control" name="university_name"> value="<?php $university_name ?>" id="university_name" placeholder="University name here">
+						      	<input type="text" class="form-control" name="university_name" value="<?php $university_name ?>" id="university_name" placeholder="University name here">
 						    </div>
 					  	</div>
 					  	<br>
@@ -80,33 +175,33 @@
 					  	<div class="form-group row">
 						    <label for="leader_contact_no" class="col-sm-3 col-form-label">Contact Number<span>*</span></label>
 						    <div class="col-sm-9">
-						      	<input type="text" class="form-control bfh-phone" data-format="ddd ddd dddd" id="leader_contact_no" name="leader_phone" value="<?php $leader_phone ?>" placeholder="Your contact number here">
+						      	<input type="text" class="form-control" id="leader_contact_no" name="leader_phone" value="<?php $leader_phone ?>" placeholder="Your contact number here">
 						    </div>
 					  	</div>
 					  	<fieldset class="form-group">
 						    <div class="row">
-						      	<legend class="col-form-label col-sm-3 pt-0">T-shirt Size</legend>
-						      	<div class="col-sm-4">
+						      	<legend class="col-form-label col-sm-3 pt-0">T-shirt Size<span>*</span></legend>
+						      	<div class="col-sm-3">
 						      		<select class="custom-select" name="leader_t_shirt">
-									  	<option selected>Not selected</option>
-									  	<option value="<?php $xs ?>">XS</option>
-									  	<option value="<?php $s ?>">S</option>
-									  	<option value="<?php $m ?>">M</option>
-									  	<option value="<?php $l ?>">L</option>
-									  	<option value="<?php $xl ?>">XL</option>
-									  	<option value="<?php $xxl ?>">XXL</option>
+						      			<option value="" selected>Choose...</option>
+									  	<option value="XS">XS</option>
+									  	<option value="S">S</option>
+									  	<option value="M">M</option>
+									  	<option value="L">L</option>
+									  	<option value="XL">XL</option>
+									  	<option value="XXL">XXL</option>
 									</select>
 						      	</div>
 						    </div>
 						  </fieldset>
 					  	<fieldset class="form-group">
 						    <div class="row">
-						      	<legend class="col-form-label col-sm-3 pt-0">Preffered Meal</legend>
-						      	<div class="col-sm-4">
+						      	<legend class="col-form-label col-sm-3 pt-0">Preffered Meal<span>*</span></legend>
+						      	<div class="col-sm-3">
 						      		<select class="custom-select" name="leader_food">
-									  	<option selected>Not selected</option>
-									  	<option value="<?php $vegi ?>">Vegi</option>
-									  	<option value="<?php $non_vegi ?>">Non-Vegi</option>
+									  	<option value="" selected>Choose...</option>
+									  	<option value="Vegi">Vegi</option>
+									  	<option value="Non-Vegi">Non-Vegi</option>
 									</select>
 						      	</div>
 						    </div>
@@ -132,28 +227,28 @@
 					  	</div>
 					  	<fieldset class="form-group">
 						    <div class="row">
-						      	<legend class="col-form-label col-sm-3 pt-0">T-shirt Size</legend>
-						      	<div class="col-sm-4">
+						      	<legend class="col-form-label col-sm-3 pt-0">T-shirt Size<span>*</span></legend>
+						      	<div class="col-sm-3">
 						      		<select class="custom-select" name="member1_t_shirt">
-									  	<option selected>Not selected</option>
-									  	<option value="<?php $xs ?>">XS</option>
-									  	<option value="<?php $s ?>">S</option>
-									  	<option value="<?php $m ?>">M</option>
-									  	<option value="<?php $l ?>">L</option>
-									  	<option value="<?php $xl ?>">XL</option>
-									  	<option value="<?php $xxl ?>">XXL</option>
+						      			<option value="" selected>Choose...</option>
+									  	<option value="XS">XS</option>
+									  	<option value="S">S</option>
+									  	<option value="M">M</option>
+									  	<option value="L">L</option>
+									  	<option value="XL">XL</option>
+									  	<option value="XXL">XXL</option>
 									</select>
 						      	</div>
 						    </div>
 						  </fieldset>
 					  	<fieldset class="form-group">
 						    <div class="row">
-						      	<legend class="col-form-label col-sm-3 pt-0">Preffered Meal</legend>
-						      	<div class="col-sm-4">
+						      	<legend class="col-form-label col-sm-3 pt-0">Preffered Meal<span>*</span></legend>
+						      	<div class="col-sm-3">
 						      		<select class="custom-select" name="member1_food">
-									  	<option selected>Not selected</option>
-									  	<option value="<?php $vegi ?>">Vegi</option>
-									  	<option value="<?php $non_vegi ?>">Non-Vegi</option>
+									  	<option value="" selected>Choose...</option>
+									  	<option value="Vegi">Vegi</option>
+									  	<option value="Non-Vegi">Non-Vegi</option>
 									</select>
 						      	</div>
 						    </div>
@@ -179,28 +274,28 @@
 					  	</div>
 					  	<fieldset class="form-group">
 						    <div class="row">
-						      	<legend class="col-form-label col-sm-3 pt-0">T-shirt Size</legend>
-						      	<div class="col-sm-4">
+						      	<legend class="col-form-label col-sm-3 pt-0">T-shirt Size<span>*</span></legend>
+						      	<div class="col-sm-3">
 						      		<select class="custom-select" name="member2_t_shirt">
-									  	<option selected>Not selected</option>
-									  	<option value="<?php $xs ?>">XS</option>
-									  	<option value="<?php $s ?>">S</option>
-									  	<option value="<?php $m ?>">M</option>
-									  	<option value="<?php $l ?>">L</option>
-									  	<option value="<?php $xl ?>">XL</option>
-									  	<option value="<?php $xxl ?>">XXL</option>
+									  	<option value="" selected>Choose...</option>
+									  	<option value="XS">XS</option>
+									  	<option value="S">S</option>
+									  	<option value="M">M</option>
+									  	<option value="L">L</option>
+									  	<option value="XL">XL</option>
+									  	<option value="XXL">XXL</option>
 									</select>
 						      	</div>
 						    </div>
 						  </fieldset>
 					  	<fieldset class="form-group">
 						    <div class="row">
-						      	<legend class="col-form-label col-sm-3 pt-0">Preffered Meal</legend>
-						      	<div class="col-sm-4">
+						      	<legend class="col-form-label col-sm-3 pt-0">Preffered Meal<span>*</span></legend>
+						      	<div class="col-sm-3">
 						      		<select class="custom-select" name="member2_food">
-									  	<option selected>Not selected</option>
-									  	<option value="<?php $vegi ?>">Vegi</option>
-									  	<option value="<?php $non_vegi ?>">Non-Vegi</option>
+									  	<option value="" selected>Choose...</option>
+									  	<option value="Vegi">Vegi</option>
+									  	<option value="Non-Vegi">Non-Vegi</option>
 									</select>
 						      	</div>
 						    </div>
@@ -226,28 +321,28 @@
 					  	</div>
 					  	<fieldset class="form-group">
 						    <div class="row">
-						      	<legend class="col-form-label col-sm-3 pt-0">T-shirt Size</legend>
-						      	<div class="col-sm-4">
+						      	<legend class="col-form-label col-sm-3 pt-0">T-shirt Size<span>*</span></legend>
+						      	<div class="col-sm-3">
 						      		<select class="custom-select" name="member3_t_shirt">
-									  	<option selected>Not selected</option>
-									  	<option value="<?php $xs ?>">XS</option>
-									  	<option value="<?php $s ?>">S</option>
-									  	<option value="<?php $m ?>">M</option>
-									  	<option value="<?php $l ?>">L</option>
-									  	<option value="<?php $xl ?>">XL</option>
-									  	<option value="<?php $xxl ?>">XXL</option>
+									  	<option value="" selected>Choose...</option>
+									  	<option value="XS">XS</option>
+									  	<option value="S">S</option>
+									  	<option value="M">M</option>
+									  	<option value="L">L</option>
+									  	<option value="XL">XL</option>
+									  	<option value="XXL">XXL</option>
 									</select>
 						      	</div>
 						    </div>
 						  </fieldset>
 					  	<fieldset class="form-group">
 						    <div class="row">
-						      	<legend class="col-form-label col-sm-3 pt-0">Preffered Meal</legend>
-						      	<div class="col-sm-4">
+						      	<legend class="col-form-label col-sm-3 pt-0">Preffered Meal<span>*</span></legend>
+						      	<div class="col-sm-3">
 						      		<select class="custom-select" name="member3_food">
-									  	<option selected>Not selected</option>
-									  	<option value="<?php $vegi ?>">Vegi</option>
-									  	<option value="<?php $non_vegi ?>">Non-Vegi</option>
+									  	<option value="" selected>Choose...</option>
+									  	<option value="Vegi">Vegi</option>
+									  	<option value="Non-Vegi">Non-Vegi</option>
 									</select>
 						      	</div>
 						    </div>
@@ -273,28 +368,28 @@
 					  	</div>
 					  	<fieldset class="form-group">
 						    <div class="row">
-						      	<legend class="col-form-label col-sm-3 pt-0">T-shirt Size</legend>
-						      	<div class="col-sm-4">
+						      	<legend class="col-form-label col-sm-3 pt-0">T-shirt Size<span>*</span></legend>
+						      	<div class="col-sm-3">
 						      		<select class="custom-select" name="member4_t_shirt">
-									  	<option selected>Not selected</option>
-									  	<option value="<?php $xs ?>">XS</option>
-									  	<option value="<?php $s ?>">S</option>
-									  	<option value="<?php $m ?>">M</option>
-									  	<option value="<?php $l ?>">L</option>
-									  	<option value="<?php $xl ?>">XL</option>
-									  	<option value="<?php $xxl ?>">XXL</option>
+									  	<option value="" selected>Choose...</option>
+									  	<option value="XS">XS</option>
+									  	<option value="S">S</option>
+									  	<option value="M">M</option>
+									  	<option value="L">L</option>
+									  	<option value="XL">XL</option>
+									  	<option value="XXL">XXL</option>
 									</select>
 						      	</div>
 						    </div>
 						  </fieldset>
 					  	<fieldset class="form-group">
 						    <div class="row">
-						      	<legend class="col-form-label col-sm-3 pt-0">Preffered Meal</legend>
-						      	<div class="col-sm-4">
+						      	<legend class="col-form-label col-sm-3 pt-0">Preffered Meal<span>*</span></legend>
+						      	<div class="col-sm-3">
 						      		<select class="custom-select" name="member4_food">
-									  	<option selected>Not selected</option>
-									  	<option value="<?php $vegi ?>">Vegi</option>
-									  	<option value="<?php $non_vegi ?>">Non-Vegi</option>
+									  	<option value="" selected>Choose...</option>
+									  	<option value="Vegi">Vegi</option>
+									  	<option value="Non-Vegi">Non-Vegi</option>
 									</select>
 						      	</div>
 						    </div>
@@ -302,7 +397,7 @@
 						<div class="divider"></div>
 						<div class="form-group row" id="reg-submit-button-div">
 						    <div class="col-sm-12">
-						        <button name="submit" id="reg-button" type="submit" class="btn btn-primary">SUBMIT</button>
+						        <button name="submit" id="reg-button" type="submit" class="btn btn-primary">REGISTER</button>
 						    </div>
 						</div>
 					</form>
