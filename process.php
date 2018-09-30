f07eb910cd1cc8121f3907a7fd60d13215d570a6
BhathiyaTK <bhathiyakariyawasam94@gmail.com>
2018-09-30 22:50:39 +0530

    set php codes to post form data to databse and add some css stylings

--- process.php
+++ process.php
@@ -1,58 +1,68 @@
 
 <?php
 
-session_start();
-
 include 'db.php';
+
+session_start();
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     if (isset($_POST["submit"])) {
         $team_name = $_POST["team_name"];
         $university_name = $_POST["university_name"];
-
         $leader_name = $_POST["leader_name"];
         $leader_email = $_POST["leader_email"];
         $leader_phone = $_POST["leader_phone"];
         $leader_t_shirt = $_POST["leader_t_shirt"];
         $leader_food = $_POST["leader_food"];
-
         $member1_name = $_POST["member1_name"];
         $member1_email = $_POST["member1_email"];
         $member1_t_shirt = $_POST["member1_t_shirt"];
-        $member1_food = $POST["member1_food"];
-
+        $member1_food = $_POST["member1_food"];
         $member2_name = $_POST["member2_name"];
         $member2_email = $_POST["member2_email"];
         $member2_t_shirt = $_POST["member2_t_shirt"];
-        $member2_food = $POST["member2_food"];
-
+        $member2_food = $_POST["member2_food"];
         $member3_name = $_POST["member3_name"];
         $member3_email = $_POST["member3_email"];
         $member3_t_shirt = $_POST["member3_t_shirt"];
-        $member3_food = $POST["member3_food"];
-
+        $member3_food = $_POST["member3_food"];
         $member4_name = $_POST["member4_name"];
         $member4_email = $_POST["member4_email"];
         $member4_t_shirt = $_POST["member4_t_shirt"];
-        $member4_food = $POST["member4_food"];
+        $member4_food = $_POST["member4_food"];
 
-        if (($team_name!="")&&($university_name!="")&&($leader_name!="")&&($leader_email!="")&&($leader_phone!="")&&($leader_t_shirt!="")&&($leader_food!="")&&($member1_name!="")&&($member1_email!="")&&($member1_t_shirt!="")&&($member1_food!="")&&($member2_name!="")&&($member2_email)&&($member2_t_shirt)&&($member2_food)&&($member3_name!="")&&($member3_email)&&($member3_t_shirt)&&($member3_food)&&($member4_name!="")&&($member4_email)&&($member4_t_shirt)&&($member4_food)) {
-                
-            $query_teams = "INSERT INTO teams(team_name,university_name) VALUES('$team_name','$university_name')";
-            $query_leaders = "INSERT INTO leaders(name,email,phone,t_shirt,meal) VALUES('$leader_name','$leader_email','$leader_phone','$leader_t_shirt','$leader_food')";
-            $query_member1 = "INSERT INTO member1(name,email,t_shirt,meal) VALUES('$member1_name','$member1_email','$member1_t_shirt','$member1_food')";
-            $query_member2 = "INSERT INTO member2(name,email,t_shirt,meal) VALUES('$member2_name','$member2_email','$member2_t_shirt','$member2_food')";
-            $query_member3 = "INSERT INTO member3(name,email,t_shirt,meal) VALUES('$member3_name','$member3_email','$member3_t_shirt','$member3_food')";
-            $query_member4 = "INSERT INTO member4(name,email,t_shirt,meal) VALUES('$member4_name','$member4_email','$member4_t_shirt','$member4_food')";
+        if (($team_name!="")&&($university_name!="")) {
+            
+            $query_teams = "INSERT INTO team_info(team_name,university_name,leader_name,leader_email,leader_phone,leader_t_shirt,leader_meal, member1_name,member1_email,member1_t_shirt,member1_meal,member2_name,member2_email,member2_t_shirt,member2_meal,member3_name,member3_email,member3_t_shirt,member3_meal,member4_name,member4_email,member4_t_shirt,member4_meal) VALUES('$team_name','$university_name','$leader_name','$leader_email','$leader_phone','$leader_t_shirt','$leader_food','$member1_name','$member1_email','$member1_t_shirt','$member1_food','$member2_name','$member2_email','$member2_t_shirt','$member2_food','$member3_name','$member3_email','$member3_t_shirt','$member3_food','$member4_name','$member4_email','$member4_t_shirt','$member4_food')";
+            
 
-            if (($conn->query($query_teams))&&($conn->query($query_leaders))&&($conn->query($query_member1))&&($conn->query($query_member2))&&($conn->query($query_member3))&&($conn->query($query_member4))) {
-                # code...
+            if ($conn->query($query_teams)) {
+
+                $registration_message = "<h2>Hi, team <b><b>$team_name</b></b> !</h2><br> <h5>Your team had been successfully registered.</h5><br><h6>Thanks for registering</h6>";
+
+                $headers = "From: bhathiyakariyawasam94@gmail.com \r\n";
+                $headers .= "Reply-To: bhathiyakariyawasam94@gmail.com \r\n";
+                $headers .= "To: $leader_email \r\n";
+                $headers .= "X-Mailer: PHP/" . PHP_VERSION;
+
+                $to = "$leader_email";
+                $subject = "Successfully registered for Virtual Rival 2k18";
+                $body =  "Hi, $leader_name!\n Your team, $team_name had been successfully registered for Virtual Rival 2k18.\n\n  Date : 27th October 2018\n Time : 1.00PM\n Venue : Sabagamuwa University Premises\n\n Get ready and warm up your team for rival. We'll hope to see you at there on the time with your team. Good luck and happy gaming!\n\n\n\n Thank you.\nOrganizing Team,\nVirtual Rival 2k18 - SUSL";
+
+                mail($to, $subject, $body, $headers);
+
+            }else{
+                $registration_message = "<h5>Registration Failed!</h5><br><h6>Check your internet connection</h6>";
             }
+        }else{
+            $registration_message = "<h5>Registration Failed!</h5><br><h6>Please fill all the required fields.</h6>";
         }
     }
 }
 
+   
 ?>
+
 
 <!DOCTYPE html>
 <html>
@@ -81,20 +91,80 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
     <link rel="icon" href="images/VR-logo-1.png">
-    <title>Response submitted</title>
+    <title>Registration succeed</title>
 </head>
 
-
+<style type="text/css">
+    .process-sec{
+        height: auto;
+        position: relative;
+    }
+    #process-sec-background{
+        padding: 50px 30px;
+        height: 100%;
+    }
+    .registration-messege-content{
+        padding: 30px 0;
+        text-align: center;
+        color: #fff;
+        font-family: Nunito;
+        background: rgba(0,0,0,0.5);
+        border-radius: 40px;
+    }
+    #process-div-logos{
+        text-align: center;
+        font-family: Nunito;
+        font-weight: bold;
+        font-size: 18px;
+    }
+    #process-div-logos-title{
+        margin-bottom: 30px;
+    }
+    #process-div-logos-title img{
+        width: auto;
+        height: 70px;
+        padding-right: 20px;
+    }
+    #process-div-logos-img img{
+        width: auto;
+        height: 70px;
+        margin: 10px 0px;
+        padding-right: 30px;
+    }
+</style>
 
 <body>
+    <div id="particles-js"></div>
     <section class="process-sec">
         <div id="process-sec-background">
-            <div>
-                
+            <div class="registration-messege-content container">
+                <?php echo $registration_message ?>
+                <div class="go-back-button">
+                    <a id="process_button" class="btn btn-primary" href="index.php" style="margin: 30px;">BACK TO HOME</a>
+                </div>
             </div>
-            <div>
-                <a id="process_button" class="btn btn-primary" href="index.php">BACK TO HOME</a>
+        </div>
+        <div class="container" id="process-div-logos">
+            <div id="process-div-logos-title">
+                <img src="images/VR-logo-1.png">Organized by
+            </div>
+            <div id="process-div-logos-img">
+                <img src="images/Logo-SUSL.png">
+                <img src="images/cis.png">
+                <img src="images/socs.png">
+                <img src="images/ieee.png">
             </div>
         </div>
     </section>
+
+    <script src="function.js"></script>
+    <script src="particles.js"></script>
+    <script src="app.js"></script>
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <script>
+        AOS.init({
+            once: true
+        });
+    </script>
+    <script src="BootstrapFormHelper/js/bootstrap-formhelpers-phone.js"></script>
 </body>
